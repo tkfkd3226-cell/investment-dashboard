42bd04d7e07fdc81e7920ac721be3a8781997199
tkfkd3226-cell <tkfkd3226@gmail.com>
2026-08-27 16:12:55 +0900

    Revert "KRX 종가차트 종목 색상 표시 정합성 보완"
    This reverts commit be4f197add5d301c1348b1b8313abe0bc58b72ff.

--- js/dashboard-charts.js
+++ js/dashboard-charts.js
@@ -919,7 +919,7 @@ function priceChartColor(account,name){
   return account==='pension'?pensionSeriesColor(name):(SECURITY_SYMBOL_COLORS[name]||securityAllocationColor(name));
 }
 function priceChartSwatch(account,name){
-  return chartSeriesSwatch(priceChartColor(account,name));
+  return account==='pension'?pensionProductSwatch(name):securitySymbolSwatch(name);
 }
 function priceChartCardItems(account,date,orderedAssets){
   const config=PRICE_CHART_ACCOUNT[account];
